f94afc4f5fb38d4a8992c3f098394fdf7d377f15
VisualProgramer <potalitcin@gmail.com>
2023-02-19 19:40:09 -0500

    First session
    Completed Task_1 and Task_2

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая создает 10 задач на выполнение некоторой работы, используя класс Task.
+//Каждая задача должна вычислять квадрат числа от 1 до 10 и выводить его на консоль.
+//Каждая задача должна быть выполнена в отдельном потоке.
+//После запуска всех задач, программа должна ждать их завершения и выводить сообщение о завершении работы каждой задачи.
+
+//=====================================================
+//Task(Action<object?> action, object? state);
+
+int count = 10;
+Random random = new Random();
+var tasks = new Task[count];
+
+for (int i = 0; i < count; i++)
+{
+    tasks[i] = new Task(() => { MyTask(random.Next(1,10)); });
+    tasks[i].Start();
+}
+
+for (int i = 0; i < count; i++)
+{
+    tasks[i].Wait();
+    Console.WriteLine($"MyTask #{i} - ended work ");
+}
+
+
+void MyTask(int number)
+{
+    Console.WriteLine(number * number);
+}
